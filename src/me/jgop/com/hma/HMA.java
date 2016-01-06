1291b6311ee408c9f61ac37f76aeefde6f868a2c
John Green <john@webindustry.co.uk>
2016-01-06 13:32:47 +0000

    Quick change

--- src/me/jgop/com/hma/HMA.java
+++ src/me/jgop/com/hma/HMA.java
@@ -3,6 +3,16 @@ package me.jgop.com.hma;
 import java.io.IOException;
 
 public class HMA {
+	public static void main(String[] args){
+		HMA hma = new HMA(OSVersion.BIT_64);
+		try {
+			hma.connect();
+			hma.changeIP();
+			hma.disconnect();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	private String path;
 	public HMA(OSVersion osVersion){
 		this.path = osVersion.getPath();
@@ -22,7 +32,7 @@ public class HMA {
 	public enum OSVersion{
 		//32 and 86 are the same but added both incase people dont know this fact
 		BIT_32("C:\\Program Files\\HMA! Pro VPN\\bin\\HMA! Pro VPN.exe"),
-		BIT64("C:\\Program Files (x86)\\HMA! Pro VPN\\bin\\HMA! Pro VPN.exe"),
+		BIT_64("C:\\Program Files (x86)\\HMA! Pro VPN\\bin\\HMA! Pro VPN.exe"),
 		BIT_86("C:\\Program Files\\HMA! Pro VPN\\bin\\HMA! Pro VPN.exe");
 		private String path;
 		OSVersion(String path){
