ec11ff700ad5da824c2a79d9f5e9eee10ff6d28e
John Green <john@webindustry.co.uk>
2016-01-06 13:25:14 +0000

    First push

---
+++ src/me/jgop/com/hma/HMA.java
@@ -0,0 +1,35 @@
+package me.jgop.com.hma;
+
+import java.io.IOException;
+
+public class HMA {
+	private String path;
+	public HMA(OSVersion osVersion){
+		this.path = osVersion.getPath();
+	}
+	public HMA(String path){
+		this.path = path;
+	}
+	public void changeIP() throws IOException{
+		Runtime.getRuntime().exec("\""+path+"\" -changeip");
+	}
+	public void connect() throws IOException{
+		Runtime.getRuntime().exec("\""+path+"\" -connect");
+	}
+	public void disconnect() throws IOException{
+		Runtime.getRuntime().exec("\""+path+"\" -disconnect");
+	}
+	public enum OSVersion{
+		//32 and 86 are the same but added both incase people dont know this fact
+		BIT_32("C:\\Program Files\\HMA! Pro VPN\\bin\\HMA! Pro VPN.exe"),
+		BIT64("C:\\Program Files (x86)\\HMA! Pro VPN\\bin\\HMA! Pro VPN.exe"),
+		BIT_86("C:\\Program Files\\HMA! Pro VPN\\bin\\HMA! Pro VPN.exe");
+		private String path;
+		OSVersion(String path){
+			this.path = path;
+		}
+		public String getPath(){
+			return path;
+		}
+	}
+}
